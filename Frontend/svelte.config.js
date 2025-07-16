7949674f6506ba60918e8d73e9f2411d332b6a1d
Dylan Dias <dylandias960@yahoo.com>
2025-07-15 21:23:57 -0400

    cmt

--- Frontend/svelte.config.js
+++ Frontend/svelte.config.js
@@ -5,7 +5,7 @@ import { fileURLToPath, URL } from 'node:url';
 const config = {
   preprocess: sveltePreprocess(),
   kit: {
-    adapter: vercel(),  // <-- This must be here, NOT adapter-auto
+    adapter: adapter(),  // Use Netlify adapter here
     alias: {
       $components: fileURLToPath(new URL('./src/lib/components', import.meta.url)),
       $Login: fileURLToPath(new URL('./src/lib/routes/Login', import.meta.url)),
