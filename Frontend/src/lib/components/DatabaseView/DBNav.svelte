3374197f679980c88885e851dccca7d8aaf9f970
Dylan Dias <dylandias960@yahoo.com>
2026-01-04 13:49:25 -0500

    new commits one time pw

--- Frontend/src/lib/components/DatabaseView/DBNav.svelte
+++ Frontend/src/lib/components/DatabaseView/DBNav.svelte
@@ -20,7 +20,6 @@
 <Header company="CowlibrateAI" platformName="Dashboard">
   <!-- Desktop Nav -->
   <HeaderNav>
-    <HeaderNavItem on:click={() => onNavigate('/cow')}>Bovine Entry</HeaderNavItem>
     <!-- <HeaderNavItem on:click={() => onNavigate('/dbview')}>Herd View</HeaderNavItem> -->
     <HeaderNavItem on:click={onGenerateReport}>Generate Report</HeaderNavItem>
     <HeaderNavItem on:click={onLogout}>Logout</HeaderNavItem>
