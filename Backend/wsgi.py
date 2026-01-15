2761aac2b5a546e39d8dccc5076ed706b192e0e5
Dylan Dias <dylandias960@yahoo.com>
2026-01-15 16:44:05 -0500

    seeing if this fixes backend issue with login ;p

--- Backend/wsgi.py
+++ Backend/wsgi.py
@@ -1,5 +1,11 @@
-from submissions import create_app  # absolute import
+from flask import Flask
+from flask_cors import CORS
+from submissions import create_app
+
 app = create_app()
 
+# Allow requests from your frontend
+CORS(app, origins=["https://cowlibrate.com"], supports_credentials=True)
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
