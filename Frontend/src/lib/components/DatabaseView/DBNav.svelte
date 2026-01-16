ec633422d565e2b9f4c1e98b6e5207aacf6ddf86
Dylan Dias <dylandias960@yahoo.com>
2026-01-16 15:41:56 -0500

    cmmits

--- Frontend/src/lib/components/DatabaseView/DBNav.svelte
+++ Frontend/src/lib/components/DatabaseView/DBNav.svelte
@@ -7,18 +7,21 @@
 
   import { goto } from "$app/navigation";
 
+  // ------------------------
+  // Props
+  // ------------------------
+  export let onLogout; // <-- Add this line
+  export let onNavigate; // If you want to use the onNavigate prop too
 
-    function navigateTo(path) {
-    goto(path);
+  function navigateTo(path) {
+    if (onNavigate) onNavigate(path);
+    else goto(path);
   }
 </script>
 
 <Header company="Dairy Dashboard" platformName="Milk Yield">
   <HeaderNav>
     <HeaderNavItem on:click={() => navigateTo("/analytics")}> Bovine Analytics </HeaderNavItem>
-   <HeaderNavItem on:click={onLogout}>Logout</HeaderNavItem>
-
-
-
+    <HeaderNavItem on:click={onLogout}>Logout</HeaderNavItem>
   </HeaderNav>
 </Header>
